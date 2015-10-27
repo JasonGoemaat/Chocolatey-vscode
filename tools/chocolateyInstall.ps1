182239f2afdb4b7b7d1a6f39533828fb8d9229ee
Jason Goemaat <jason.goemaat@gmail.com>
2015-10-26 23:37:21 -0500

    Initial Commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'vscode'
+$fileType = 'exe'
+$url = 'https://az764295.vo.msecnd.net/public/0.9.2/VSCodeSetup.exe'
+$silentArgs = '/silent'
+
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"
